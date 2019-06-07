161789db4c782192b84ca0b7240460be46bc9c67
Ryan <rrobertson@gmail.com>
2019-06-06 22:05:09 -0700

    clean up naming, pos only

--- src/fp_add.sv
+++ src/fp_add.sv
@@ -8,16 +8,17 @@ module fp_add (
   output logic [31:0] sum
 );
 
-  logic [7:0] exp_a;
-  logic [7:0] exp_b;
-  logic [7:0] exp_a_norm;
-  logic [7:0] exp_b_norm;
-  logic [7:0] exp_a_norm_q;
-  logic [7:0] exp_b_norm_q;
-  logic [7:0] exp_a_norm_q2;
-  logic [7:0] exp_b_norm_q2;
+  logic sign_bit_q;
+  logic sign_bit_q2;
+  logic sign_bit_q3;
 
-  logic       exp_a_larger;
+  logic [7:0] a_exp;
+  logic [7:0] b_exp;
+  logic [7:0] exp_norm;
+  logic [7:0] exp_norm_q;
+  logic [7:0] exp_norm_q2;
+
+  logic       a_exp_larger;
 
   logic [7:0] exp_diff;
 
@@ -32,8 +33,8 @@ module fp_add (
 
   logic [2:0] sum_vld_sr;
 
-  assign exp_a = a[30:23];
-  assign exp_b = b[30:23];
+  assign a_exp = a[30:23];
+  assign b_exp = b[30:23];
 
   always_ff @(posedge clk) begin
     sum_vld_sr <= {sum_vld_sr[$size(sum_vld_sr)-2:0], in_vld};
@@ -41,32 +42,28 @@ module fp_add (
     b_mant_q   <= b[22:0];
 
     // 1
-    if (exp_a >= exp_b) begin
-      exp_a_larger <= 1;
-      exp_b_norm   <= exp_a;
-      exp_a_norm   <= exp_a;
-      exp_diff     <= exp_a - exp_b;
+    if (a_exp >= b_exp) begin
+      a_exp_larger <= 1;
+      exp_norm     <= b_exp;
+      exp_diff     <= a_exp - b_exp;
     end else begin
-      exp_a_larger <= 0;
-      exp_a_norm   <= exp_b;
-      exp_b_norm   <= exp_b;
-      exp_diff     <= exp_b - exp_a;
+      a_exp_larger <= 0;
+      exp_norm     <= a_exp;
+      exp_diff     <= b_exp - a_exp;
     end
 
     // 2
-    exp_a_norm_q <= exp_a_norm;
-    exp_b_norm_q <= exp_b_norm;
-    a_mant_norm  <= (exp_a_larger || exp_diff == 0)  ? {1'b1, a_mant_q} : {1'b1, a_mant_q} >> exp_diff;
-    b_mant_norm  <= (!exp_a_larger || exp_diff == 0) ? {1'b1, b_mant_q} : {1'b1, b_mant_q} >> exp_diff;
+    exp_norm_q   <= exp_norm;
+    a_mant_norm  <= (a_exp_larger || exp_diff == 0)  ? {1'b1, a_mant_q} : {1'b1, a_mant_q} >> exp_diff;
+    b_mant_norm  <= (!a_exp_larger || exp_diff == 0) ? {1'b1, b_mant_q} : {1'b1, b_mant_q} >> exp_diff;
 
     // 3
-    exp_a_norm_q2                        <= exp_a_norm_q;
-    exp_b_norm_q2                        <= exp_b_norm_q;
+    exp_norm_q2                          <= exp_norm_q;
     {hidden_ovfl, hidden_mant, sum_mant} <= a_mant_norm + b_mant_norm;
 
     // 4
     sum_vld <= sum_vld_sr[$size(sum_vld_sr)-1];
-    sum     <= hidden_ovfl ? {1'b0, exp_a_norm_q2 + 1, sum_mant >> 1} : {1'b0, exp_a_norm_q2, sum_mant};
+    sum     <= hidden_ovfl ? {1'b0, exp_norm_q2 + 1, sum_mant >> 1} : {1'b0, exp_norm_q2, sum_mant};
   end
 
 endmodule
